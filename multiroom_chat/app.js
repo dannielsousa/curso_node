5024274fc409e72aef131f5a8f99fad0f8d992f7
Danniel Sousa <danniel.sousa@cedrotech.com>
2019-02-03 18:17:47 -0200

    Alterações

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -2,6 +2,52 @@
 var app = require('./config/server.js') //pode ocultar a extensão na importação do modulo
 
 /*parametrizar a porta de esuta*/ 
-app.listen(80, function(){
+var server = app.listen(80, function(){
     console.log('Servidor Online')
+})
+
+var io = require('socket.io').listen(server);
+app.set('io', io);
+
+//criar conexão por websocket
+io.on('connection', function(socket) {
+    console.log('Usuário conectou!');
+
+    socket.on('disconnect', function() {
+        console.log('Usuário desconectou');
+        //socket.emit('usuarioDesconectou': )
+    })
+
+    socket.on('msgParaServidor', function(data) {
+        console.log('to aqui');
+        
+        socket.emit(
+            'msgParaCliente', {
+                apelido: data.apelido,
+                mensagem: data.mensagem
+            }
+        )
+        
+        socket.broadcast.emit(
+            'msgParaCliente', {
+                apelido: data.apelido,
+                mensagem: data.mensagem
+            }
+        )
+
+        /* atulizar participantes */
+
+        if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            console.log(data.apelido_atualizado_nos_clientes)
+
+            socket.emit(
+                'participantesParacliente', { apelido: data.apelido }
+            )
+                
+            socket.broadcast.emit(
+                'participantesParacliente', { apelido: data.apelido }
+            )
+        }
+
+    })
 })
