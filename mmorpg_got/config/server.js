11accf544375fcbdd08cf940c3975555fa199e96
Danniel Sousa <danniel.sousa@cedrotech.com>
2019-04-09 00:44:50 -0300

    Criacao da pagina de login e adicao do express session

--- mmorpg_got/config/server.js
+++ mmorpg_got/config/server.js
@@ -10,6 +10,9 @@ var bodyParser = require('body-parser');
 /* importar o módulo do express-validator */
 var expressValidator = require('express-validator');
 
+/* importar o módulo do express-session */
+var expressSession = require('express-session');
+
 /* iniciar o objeto do express */
 var app = express();
 
@@ -25,6 +28,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 /* configurar o middleware express-validator */
 app.use(expressValidator());
+
+/* configurar o middleware express-session */
+app.use(expressSession({
+  secret: '', // segredo para assinar o cookie de sessão
+  resave: false, // se for true faz com que a sessão seja regravada no lado do servidor
+  saveUninitialized: false // se for true cria uma sessão nova sempre que for mordificada
+}))
 
 /* efetua o autoload das rotas, dos models e dos controllers para o objeto app */
 consign()
